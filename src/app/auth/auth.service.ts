5f06320818179b38e2829c4c471a3bc34b804046
David <yandridavidr@gmail.com>
2025-06-10 16:16:36 -0500

    vercel config

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { environment } from '../../enviroment/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient, private router: Router) {}
 
---
+++ src/enviroment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ruleta-backend-0gt5.onrender.com/api/spin' // URL real en producción
+};
+
